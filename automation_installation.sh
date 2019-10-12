5e1d4fa3365c47fa08d0472877b453d29803345e
ahmedhamada <am2627@fayoum.edu.eg>
2019-10-12 12:55:00 +0200

    s

--- automation_installation.sh
+++ automation_installation.sh
@@ -41,6 +41,7 @@ sudo apt upgrade -y
 
 wget https://dl.google.com/go/go1.13.1.linux-amd64.tar.gz
 tar -C /usr/local -xzf go1.13.1.linux-amd64.tar.gz
+rm go1.13.1.linux-amd64.tar.gz
 
 export PATH=$PATH:/usr/local/go/bin
 echo -e "\nexport PATH=\$PATH:/usr/local/go/bin" >> ~/.bashrc
